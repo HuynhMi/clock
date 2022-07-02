8f5680b81611319cc4fbe82d51c1561f119b90e5
HuynhMi <54268240+HuynhMi@users.noreply.github.com>
2022-07-02 16:13:45 +0700

    commit

--- app.js
+++ app.js
@@ -1,43 +1,39 @@
+/*
+1. css for numbers
+2. css for hand
+3.setTimeout để đồng hồ chạy
+*/
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const numberElements = $$('.number');
+const hourHandElement = $('#hand-hour');
+const minuteHandElement = $('#hand-minute');
+const secondHandElement = $('#hand-second');
 
+displayNumbers= function() {
+    let deg = 0;
+    numberElements.forEach(function(element) {
+        element.style.transform = `rotate(${deg}deg)`
+        deg += 30;
+    })
+},
 
-const secondHand = document.querySelector('[data-second-hand]');
-const minuteHand = document.querySelector('[data-minute-hand]');
-const hourHand = document.querySelector('[data-hour-hand]');
+displayNumbers();
 
-//Way 1
-// function setClock() {
-//     const currentDate = new Date();
-//     const deg = 6;
-//     const secondRotate = currentDate.getSeconds() * deg;
-//     const minuteRotate = currentDate.getMinutes() * deg;
-//     const hourRotate = currentDate.getHours() * 30;
-//     setRotate(secondHand,secondRotate);
-//     setRotate(minuteHand,minuteRotate);
-//     setRotate(hourHand, hourRotate + minuteRotate/12);
-// }
+function handleTimer() {
+    const date = new Date();
+    let [hours, minutes, second] = [date.getHours(), date.getMinutes(), date.getSeconds()];
+    let hourDegs = hours*(360/12) + minutes*1/2;
+    hourHandElement.style.transform = `rotate(${hourDegs}deg)`;
+    let minutesDeg = minutes*(360/60);
+    minuteHandElement.style.transform = `rotate(${minutesDeg}deg)`;
+    let secondsDeg = second*(360/60) + 6;
+    secondHandElement.style.transform = `rotate(${secondsDeg}deg)`;
+};
 
-// function setRotate(element, rotate) {
-//     const numberDeg = `${rotate}deg`;
-//     element.style.setProperty('--rotate',numberDeg);
-// }
+function getRotate() {
 
-
-// way 2
-function setClock() {
-    const currentDate = new Date();
-    const secondRotate = currentDate.getSeconds()/60;
-    const minuteRotate = currentDate.getMinutes()/60;
-    const hourRotate = (minuteRotate + currentDate.getHours())/12;
-    setRotate(secondHand, secondRotate);
-    setRotate(minuteHand, minuteRotate);
-    setRotate(hourHand, hourRotate);
 }
-
-function setRotate(element, rotate) {
-    let deg = rotate * 360;
-    deg = `${deg}deg`;
-    console.log(deg);
-    element.style.setProperty('--rotate', deg);
-}
-setClock();
-setInterval(setClock, 1000);
+setInterval(function() {
+    handleTimer()
+}, 500);
