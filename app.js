37da495305023a191e50a7bc75857e9b6ce25a55
HuynhMi <54268240+HuynhMi@users.noreply.github.com>
2022-07-02 17:06:10 +0700

    commit

--- app.js
+++ app.js
@@ -10,27 +10,31 @@ const hourHandElement = $('#hand-hour');
 const minuteHandElement = $('#hand-minute');
 const secondHandElement = $('#hand-second');
 
-displayNumbers= function() {
-    let deg = 0;
-    numberElements.forEach(function(element) {
-        element.style.transform = `rotate(${deg}deg)`
-        deg += 30;
-    })
-},
+const app = {
+    displayNumbers : function() {
+        let deg = 0;
+        numberElements.forEach(function(element) {
+            element.style.transform = `rotate(${deg}deg)`
+            deg += 30;
+        })
+    },
+    handleTimer: function() {
+        const date = new Date();
+        let [hours, minutes, second] = [date.getHours(), date.getMinutes(), date.getSeconds()];
+        let hourDegs = hours*(360/12) + minutes*1/2;
+        hourHandElement.style.transform = `rotate(${hourDegs}deg)`;
+        let minutesDeg = minutes*(360/60);
+        minuteHandElement.style.transform = `rotate(${minutesDeg}deg)`;
+        let secondsDeg = second*(360/60);
+        secondHandElement.style.transform = `rotate(${secondsDeg}deg)`;
+    },
+    start: function() {
+        _this = this;
+        _this.displayNumbers();
+        setInterval(function() {
+            _this.handleTimer();
+        }, 500);
+    }
+}
 
-displayNumbers();
-
-function handleTimer() {
-    const date = new Date();
-    let [hours, minutes, second] = [date.getHours(), date.getMinutes(), date.getSeconds()];
-    let hourDegs = hours*(360/12) + minutes*1/2;
-    hourHandElement.style.transform = `rotate(${hourDegs}deg)`;
-    let minutesDeg = minutes*(360/60);
-    minuteHandElement.style.transform = `rotate(${minutesDeg}deg)`;
-    let secondsDeg = second*(360/60);
-    secondHandElement.style.transform = `rotate(${secondsDeg}deg)`;
-};
-
-setInterval(function() {
-    handleTimer()
-}, 500);
+app.start();
